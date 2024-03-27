5cee1f5947fadc6c00f209d4dcbd0d5e19047b98
tricyvic <muhitavictor69@gmail.com>
2024-03-27 12:04:10 +0300

    add homepage.html

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
     let loggedUser = [{username:'admin' ,password:'12334',name:'admin'},{username:'user1' ,password:'56778',name:'user1'},{username:'user2' ,password:'901223',name:'user2'},{username: 'user3' ,password: '45667',name: 'user3'},{username: 'user4' ,password: '8901223',name: 'user4'}]
     const username = document.getElementById('username');
     const password = document.getElementById('password');
+    const welcome =  document.getElementById('welcome');
+    const welcomeButton = document.getElementById('welcomeButton');
     let currentUser = '';
     let currentPassword =  '';
 
@@ -12,6 +14,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         if (loggedUser[i].username === username.value && loggedUser[i].password === password.value) {
            currentUser =  loggedUser[i].name;
            console.log(currentUser);
+        //    welcome.innerText = `Welcome ${currentUser}to this App!`;
+            welcome.innerText = `Welcome ${currentUser}to this App!`
+           welcomeButton.style = "display:block;"         
            alert(`hi there ${currentUser}`);
         }
     }
